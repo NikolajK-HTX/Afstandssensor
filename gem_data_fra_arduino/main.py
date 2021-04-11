93170673497dc2315872c84171f596ae6004cb15
Nikolaj J-K <kagenogmig@gmail.com>
2021-04-11 16:51:21 +0200

    Create main.py

---
+++ gem_data_fra_arduino/main.py
@@ -0,0 +1,26 @@
+import serial
+import csv
+
+# COM3 skal ændres efter hvilken port Arduino Unoen sidder i
+ser = serial.Serial('COM3', 57600, timeout=1) 
+
+print('Tryk på enter for at starte indsamlingen af data :-)')
+input()
+ser.write(b'Start\n')
+
+print('Programmet er nu i gang med at indsamle data. Vent venligst.\n')
+with open('outputfile.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    outputWriter = csv.writer(csvfile, delimiter=';')
+    for i in range(0, 510):
+        outputValue = str(ser.readline(), 'utf-8')[:-1]
+        if (outputValue == 'Done'):
+            print("Sensoren siger den er færdig med at indsamle data.")
+            break
+        outputValue = outputValue.split(';')
+        timeValue = float(outputValue[0])/1000
+        distanceValue = outputValue[1]
+        
+        outputWriter.writerow([timeValue, distanceValue])
+
+print('Programmet er færdigt med at indsamle data og den indsamlede data')
+print('kan findes i "outputfile.csv"')
